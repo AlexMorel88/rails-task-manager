f617d8a14edc4556433029d246b5ad9fd7e4bc9c
alexandre morel <morel.alx@gmail.com>
2017-05-16 12:07:05 +0200

    version1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  #get 'task/index'
+  #get 'task/show'
+  #get 'task/new'
+  #get 'task/create'
+  #get 'task/edit'
+  #get 'task/update'
+  #get 'task/destroy'
+  #get 'task/resources'
+  resources :tasks #remplace tout ce qu'il y a au dessus
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
